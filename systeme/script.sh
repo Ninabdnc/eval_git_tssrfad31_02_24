5613c6a220d5a8fecbf048976560b3f128d2991d
ninabdnc <ninabourdonnec@gmail.com>
2024-12-17 13:55:08 +0100

    Ajout du script.sh

---
+++ systeme/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Où voulez-vous enregistrer le projet ?"
+read directory
+echo "Quel est le nom de votre projet ?"
+read project
+cd $directory
+mkdir $project
+touch index.html style.css readme.md
+echo "Le projet a été ajouté"
